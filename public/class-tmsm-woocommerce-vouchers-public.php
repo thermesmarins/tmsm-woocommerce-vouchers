ace51fbd48f0ffcafd28d5f09181427fe59cc00f
Nico Mollet <nico.mollet@gmail.com>
2018-08-09 08:58:21 +0200

    Remove useless comments

--- public/class-tmsm-woocommerce-vouchers-public.php
+++ public/class-tmsm-woocommerce-vouchers-public.php
@@ -62,17 +62,6 @@ class Tmsm_Woocommerce_Vouchers_Public {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Tmsm_Woocommerce_Vouchers_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Tmsm_Woocommerce_Vouchers_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/tmsm-woocommerce-vouchers-public.css', array(), $this->version,
 			'all' );
@@ -85,18 +74,6 @@ class Tmsm_Woocommerce_Vouchers_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Tmsm_Woocommerce_Vouchers_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Tmsm_Woocommerce_Vouchers_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/tmsm-woocommerce-vouchers-public.js', array( 'jquery' ),
 			$this->version, true );
